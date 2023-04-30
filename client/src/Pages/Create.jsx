fbe07640125444381108f87e5e7aa7de48314050
Omar Abdelhamid <omar.hamid@zacloud.net>
2023-04-30 13:52:04 +0300

    update1

--- client/src/Pages/Create.jsx
+++ client/src/Pages/Create.jsx
@@ -11,7 +11,7 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import styles from '../Components/Layout.module.css'
+import styles from '../Components/Layout.module.css';
 export default function Create() {
     const style = {
         marginTop: 3,
@@ -40,92 +40,98 @@ export default function Create() {
             const result = await axios.post('/notes', {
                 title: title,
                 details: details,
-                category:category
+                category: category,
             });
             if (result.data.message === 'Note added to DB successfully') {
                 navigate('/');
             }
-           
         }
     };
     return (
         <>
-            <Container >
+            <Container>
                 <div className={styles.handlecontainer}>
-                <form className={styles.formwidth} onSubmit={handleSubmit} noValidate autoComplete="off">
-                    <Typography variant="h6" color="textSecondary" gutterBottom>
-                        Create a New Note
-                    </Typography>
-                    <TextField
-                        onChange={(e) => {
-                            setTitle(e.target.value);
-                        }}
-                        sx={style}
-                        label="note title"
-                        variant="outlined"
-                        color="primary"
-                        fullWidth
-                        required
-                        error={titlError}
-                    />
-                    <TextField
-                        onChange={(e) => {
-                            setDetails(e.target.value);
-                        }}
-                        sx={style}
-                        label="note details"
-                        variant="outlined"
-                        color="primary"
-                        fullWidth
-                        required
-                        multiline
-                        rows={3}
-                        error={detailsError}
-                    />
-
-                    <FormControl sx={style}>
-                        <FormLabel>Note Category</FormLabel>
-                        <RadioGroup
-                            value={category}
-                            onChange={(e) => {
-                                setCategory(e.target.value);
-                            }}
-                        >
-                            <FormControlLabel
-                                control={<Radio />}
-                                value="todos"
-                                label="Todos"
-                            />
-                            <FormControlLabel
-                                control={<Radio />}
-                                value="money"
-                                label="Money"
-                            />
-                            <FormControlLabel
-                                control={<Radio />}
-                                value="reminders"
-                                label="Reminders"
-                            />
-                            <FormControlLabel
-                                control={<Radio />}
-                                value="work"
-                                label="Work"
-                            />
-                        </RadioGroup>
-                    </FormControl>
-
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        
-                        endIcon={<KeyboardArrowRightIcon />}
+                    <form
+                        className={styles.formwidth}
+                        onSubmit={handleSubmit}
+                        noValidate
+                        autoComplete="off"
                     >
-                        Add Note
-                    </Button>
-                </form>
+                        <Typography
+                            variant="h6"
+                            color="textSecondary"
+                            gutterBottom
+                        >
+                            Create a New Note
+                        </Typography>
+                        <TextField
+                            onChange={(e) => {
+                                setTitle(e.target.value);
+                            }}
+                            sx={style}
+                            label="note title"
+                            variant="outlined"
+                            color="primary"
+                            fullWidth
+                            required
+                            error={titlError}
+                        />
+                        <TextField
+                            onChange={(e) => {
+                                setDetails(e.target.value);
+                            }}
+                            sx={style}
+                            label="note details"
+                            variant="outlined"
+                            color="primary"
+                            fullWidth
+                            required
+                            multiline
+                            rows={3}
+                            error={detailsError}
+                        />
+
+                        <FormControl sx={style}>
+                            <FormLabel>Note Category</FormLabel>
+                            <RadioGroup
+                                value={category}
+                                onChange={(e) => {
+                                    setCategory(e.target.value);
+                                }}
+                            >
+                                <FormControlLabel
+                                    control={<Radio />}
+                                    value="todos"
+                                    label="Todos"
+                                />
+                                <FormControlLabel
+                                    control={<Radio />}
+                                    value="money"
+                                    label="Money"
+                                />
+                                <FormControlLabel
+                                    control={<Radio />}
+                                    value="reminders"
+                                    label="Reminders"
+                                />
+                                <FormControlLabel
+                                    control={<Radio />}
+                                    value="work"
+                                    label="Work"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            endIcon={<KeyboardArrowRightIcon />}
+                        >
+                            Add Notes
+                        </Button>
+                    </form>
                 </div>
-            
             </Container>
         </>
     );
