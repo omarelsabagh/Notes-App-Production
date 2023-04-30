fec822095addce9a571cdf24916fe8be825aaf2a
Omar Abdelhamid <omar.hamid@zacloud.net>
2023-04-30 15:38:58 +0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,45 @@
 import express from 'express';
 import dotenv from 'dotenv';
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
+const { Octokit } = require('@octokit/core');
+const octokit = new Octokit({
+    auth: 'zaclouds-test-secret',
+});
 
+const result = await octokit.request(
+    'GET /repos/omarelsabagh/Notes-App-Production',
+    {
+        owner: 'omarelsabagh',
+        repo: 'Notes-App-Production',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+        },
+    }
+);
+
+console.log(result.data);
 dotenv.config();
 
 import { fetchAllRoutes } from './routes/allRoutes';
 
 export const app: express.Application = express();
 
-app.use(express.static(path.join(__dirname,"../client/build")))
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(cors());
-app.use(function(_req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (_req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+    res.header(
+        'Access-Control-Allow-Headers',
+        'x-access-token, Origin, X-Requested-With, Content-Type, Accept'
+    );
     next();
-  });
+});
 //env variable for the port
 
 const port = process.env.PORT || 5000;
@@ -25,8 +47,9 @@ const port = process.env.PORT || 5000;
 // allRoutes file in routes folder
 fetchAllRoutes(app);
 
-app.get('*',(_req,res)=>{
-  res.sendFile(path.join(__dirname,"../client/build/index.html"))
-})
-app.listen(port,()=>{console.log(`server is running on port: ${port}`);
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
 });
--- test-hooks.js
+++ test-hooks.js
@@ -1,18 +1,2 @@
 // Octokit.js
 // https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-    auth: 'YOUR-TOKEN',
-});
-
-const result = await octokit.request(
-    'GET /repos/omarelsabagh/Notes-App-Production',
-    {
-        owner: 'omarelsabagh',
-        repo: 'Notes-App-Production',
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28',
-        },
-    }
-);
-
-console.log(result.data);
