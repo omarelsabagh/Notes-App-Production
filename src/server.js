51c95d28d0e8bf8bef30d5aa3fb21f06328ce301
Omar Abdelhamid <omar.hamid@zacloud.net>
2023-05-01 08:47:07 +0300

    test

--- src/server.js
+++ src/server.js
@@ -7,5 +7,16 @@ webhooks.onAny(({ id, name, payload }) => {
     console.log(name, 'event received');
 });
 
-require('http').createServer(createNodeMiddleware(webhooks)).listen(80);
+// require('http').createServer(createNodeMiddleware(webhooks)).listen(80);
 // can now receive webhook events at /api/github/webhooks
+///////////////////////////////////////////////////////
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(
+    createNodeMiddleware(webhooks),
+    function (req, res) {
+        // 2 - creating server
+    }
+);
+
+server.listen(80); //3 - listen for any incoming requests
