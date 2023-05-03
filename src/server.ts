b49c0b4677abf89c78cd4e5345204050aaeb4d4f
Omar Abdelhamid <omar.hamid@zacloud.net>
2023-05-03 12:07:15 +0300

    test

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { Octokit } from '@octokit/core';
 import { Webhooks, createNodeMiddleware } from '@octokit/webhooks';
 import ngrok from 'ngrok';
 const octokit = new Octokit();
-const strinog = 'ggwpe';
+const strinog = 'ggwpez';
 const webhooks = new Webhooks({
     secret: 'zaclouds-test-secret',
 });
